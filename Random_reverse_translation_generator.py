aa6e85f08937d6244b661e74ddd5f4434e674fd3
chillwei <52937421+chillwei@users.noreply.github.com>
2022-10-18 13:01:27 -0500

    Update Random_reverse_translation_generator.py

--- Random_reverse_translation_generator.py
+++ Random_reverse_translation_generator.py
@@ -9,7 +9,23 @@ from dnachisel import *
 from dnachisel import biotools
 import flametree
 
-protein = "MAWWL*"
-test = biotools.reverse_translate(protein,True,table = 'Bacterial')
-print(test)
-# GGTCATATTTTAAAAATGTTTCCT
+# loop for generating random reverse sequences for n times
+Red = "MSKIKGEEDNMLIIKEFMRFKAHMEGSVNGHEFEIEGEGEGHPYEGTQTAKLKVTKGGPLPFAWDILSPQFMYGSKAYVKHPADIPDYLKLSFPEGFTWERVMNFEDGGVVTVTQDSSLQDGQFIYKVKLLGINFPSDGPVMQKKTMGWEASTERMYPEDGALKGEINQRLKLKDGGHYDAEVKTTYKAKKPVQLPGAYNVDIKLDITSHNEDYTIVEQYERAEGRHSTHHHHHH*"
+num = 20
+list_ID = []
+list_seq = []
+for i in range(0,num):
+    Redram = biotools.reverse_translate(Red,True,table = 'Bacterial')
+    ID = 'Red'+ str(i+1)
+    #print(ID)
+    list_ID.append(ID)
+    list_seq.append(Redram)
+    
+# write into fasta file
+ofile = open("Random_reverse.fasta", "w")
+
+for i in range(len(list_seq)):
+
+    ofile.write(">" + list_ID[i] + "\n" +list_seq[i] + "\n")
+
+ofile.close()
